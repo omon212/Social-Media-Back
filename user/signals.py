446750e643813bc37f53be7aeb890f6972330d8f
omonnuloraimkulov@gmail.com <omonnuloraimkulov@gmail.com>
2024-08-08 04:15:28 +0500

    add signals.py

--- user/signals.py
+++ user/signals.py
@@ -5,7 +5,11 @@ import os
 
 
 @receiver(post_delete, sender=UserModel)
-def delete_stadion_photo(sender, instance, **kwargs):
-    if instance.user_image:
-        if os.path.isfile(instance.user_image.path):
-            os.remove(instance.user_image.path)
+def delete_user_image(sender, instance, **kwargs):
+    if instance.user_image and instance.user_image.name != 'users_images/default.jpg':
+        image_path = instance.user_image.path
+        if os.path.isfile(image_path):
+            try:
+                os.remove(image_path)
+            except OSError as e:
+                print(f'Error: {e.strerror}')
